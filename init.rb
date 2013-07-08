0efc9403dd57fbb1ab8c3e6e260eb923b4edc068
Iceskysl <iceskysl@gmail.com>
2013-07-08 23:14:35 +0800

    fix nil.foce_encoding

--- init.rb
+++ init.rb
@@ -6,6 +6,12 @@ Redmine::Plugin.register :worklogs do
   url 'https://github.com/IceskYsl/worklogs'
   author_url 'http://my.eoe.cn/iceskysl'
   
+  # project_module :worklogs do
+  #   permission :all_worklogs, :worklogs => :index
+  #   permission :my_worklogs, :worklogs => :my
+  #   permission :new_worklogs, :worklogs => :new
+  # end
+  
   menu :top_menu, :worklogs, { :controller => 'worklogs', :action => 'index' }, :caption => :label_worklog
   menu :application_menu, :worklogs, { :controller => 'worklogs', :action => 'index' }, :caption => :label_worklog
   menu :application_menu, :worklogs_my, { :controller => 'worklogs', :action => 'my'}, :caption => :label_worklog_my
