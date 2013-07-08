bd7ff31e8eb2236cebc34752e778f33b9f7e5d57
Iceskysl <iceskysl@gmail.com>
2013-07-09 00:08:44 +0800

    修改导航栏，添加权限

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :worklogs do
   name 'Worklogs'
   author 'Iceskysl'
   description 'This is a worklogs plugin for Redmine'
-  version '0.2.0'
+  version '0.2.1'
   url 'https://github.com/IceskYsl/worklogs'
   author_url 'http://my.eoe.cn/iceskysl'
   
@@ -13,9 +13,11 @@ Redmine::Plugin.register :worklogs do
   # end
   
   menu :top_menu, :worklogs, { :controller => 'worklogs', :action => 'index' }, :caption => :label_worklog
-  menu :application_menu, :worklogs, { :controller => 'worklogs', :action => 'index' }, :caption => :label_worklog
-  menu :application_menu, :worklogs_my, { :controller => 'worklogs', :action => 'my'}, :caption => :label_worklog_my
-  menu :application_menu, :worklogs_new, { :controller => 'worklogs', :action => 'new' }, :caption => :label_worklog_new, :last => true
+
+  # menu :application_menu, :worklogs, { :controller => 'worklogs', :action => 'index' }, :caption => :label_worklog
+  # menu :application_menu, :worklogs_my, { :controller => 'worklogs', :action => 'my'}, :caption => :label_worklog_my ,:if => Proc.new { params[:controller] == "worklogs" }
+  # menu :application_menu, :worklogs_new, { :controller => 'worklogs', :action => 'new' }, :caption => :label_worklog_new, :last => true
+  # menu :project_menu, :worklogs, { :controller => 'worklogs', :action => 'new' }, :caption => :label_worklog, :before => :activity, :param => :project_id
   
   settings :default => {'empty' => true}, :partial => 'settings/worklogs_settings'
 end
