ef05dcdc6a5774fe733eb4b53f7a7cb2e2e8eed8
Iceskysl <iceskysl@gmail.com>
2013-10-28 23:07:29 +0800

    修改周报为目标

--- app/models/worklog.rb
+++ app/models/worklog.rb
@@ -4,10 +4,30 @@ class Worklog < ActiveRecord::Base
   unloadable
   belongs_to :author, :class_name => "User", :foreign_key => "user_id"
   
+  attr_accessor :plan,:plan_done
   
   TYPEE = {"day" => 0,"week" => 1} #, "month" => 2
   SCORE = {"1分（最差）" => 1, "2分" => 2, "3分" => 3, "4分" => 4, "5分" => 5, "6分（及格）" => 6, "7分" => 7, "8分" => 8, "9分" => 9, "10分（最好）" => 10}
   
+
+  
+  def plan  
+    self.todo
+  end
+  
+  def plan= plan
+    self.todo = plan
+  end
+
+  def plan_done= plan_done
+     self.do = plan_done
+  end
+
+  
+  def plan_done 
+     self.do
+  end
+
   
   def self.typee_collection
       TYPEE.collect { |s| [s[0], s[1]]}
@@ -16,6 +36,7 @@ class Worklog < ActiveRecord::Base
   def self.score_collection
       SCORE.collect { |s| [s[0], s[1]]}
   end
+  
   
   def self.no_need_users_ids
     #format: 1,2
