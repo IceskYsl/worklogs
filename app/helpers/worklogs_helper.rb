0e59b771af4063cae21118b96b8229ff4fc65f13
iceskysl <iceskysl@gmail.com>
2014-11-09 21:10:18 +0800

    add worklog modify for week

--- app/helpers/worklogs_helper.rb
+++ app/helpers/worklogs_helper.rb
@@ -3,7 +3,7 @@ module WorklogsHelper
 
   
   def can_edit?(worklog)
-    if (worklog.day == Date.today.to_s) && (worklog.user_id == session[:user_id])
+    if ((worklog.day == Date.today.to_s) || (worklog.week == Date.today.strftime("%W").to_i)) && (worklog.user_id == session[:user_id])
       return true
     end
       return false
